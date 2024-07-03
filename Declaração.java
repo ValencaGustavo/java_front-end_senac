1e9dee8add095f5c9568b619a34838ae95ccc85d
Gustavo Valença Marcilio <workgustavo10@gmail.com>
2024-07-03 16:57:53 -0300

    Add files via upload

---
+++ Declaração.java
@@ -0,0 +1,33 @@
+public class Declaração { //Declaração da classe DECLARAÇÃO
+    public static void main(String[] args) throws Exception {
+        System.out.println("Operador de atribuição: =");
+        String texto = "Texto";
+        System.out.println("String texto = \"Texto\";");
+        System.out.println(texto); // Declara o valor da variável 'texto'
+
+        System.out.println("Operador de soma: +");
+        int soma = 1 + 1; // Declaração de variável
+        System.out.println("int soma = 1 + 1;");
+        System.out.println(soma); // Declara o valor da variável 'soma'
+
+        System.out.println("Operador de subtração: -");
+        int subtracao = 1 - 1;
+        System.out.println("int subtracao = 1 - 1;");
+        System.out.println(subtracao); // Declara o valor da variável 'substracao'
+
+        System.out.println("Operador de divisão: /");
+        int divisao = 1 / 1;
+        System.out.println("int divisao = 1 / 1;");
+        System.out.println(divisao); // Declara o valor da variável 'divisao'
+
+        System.out.println("Operador de multiplicação: *");
+        int multiplicacao = 1 * 1;
+        System.out.println("int multiplicacao = 1 * 1;"); // Declara a operação de multiplicação
+        System.out.println(multiplicacao); // Declara o valor da variável 'multiplicacao'
+
+        System.out.println("Operador de concatenação de strings: +");
+        String concatenacao = "Texto 1 + " + "Texto 2";
+        System.out.println("String concatenacao = \"Texto 1 + \" + \"Texto 2\";"); // Concatenação de strings
+        System.out.println(concatenacao); // Declara o valor da variável 'concatenacao'
+    }
+}
