bce1074d2714c1040278e8ff95d3194f639fc7ac
Gustavo Valença Marcilio <workgustavo10@gmail.com>
2024-07-04 15:11:24 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,22 @@
+public class Variaveis {
+
+    public static void main(String[] args) throws Exception {
+        int varInt = 2; // Declaração de variável varInt, do tipo int, com valor recebido 2
+        System.out.println("Variável do tipo int (inteiro): " + varInt);
+    
+        Integer varInteger = 3; // Declaração de variável varInteger, do tipo int, com valor recebido 3
+        System.out.println("Variável do tipo Integer (inteiro): " + varInteger);
+    
+        String varString = "texto"; // Declaração de variável varString, do tipo string, recebido com texto
+        System.out.println("Variável do tipo String (texto): " + varString);
+    
+        boolean varBoolean = true; // Declaração de variável varBoolean, do tipo Boolean, verifica se é verdadeiro ou falso
+        System.out.println("Variável do tipo Boolean ()");
+    
+        byte varByte = 1;
+    
+        char varChar = 'r';
+    
+        float varFloat = 19.99f;
+    }
+}
