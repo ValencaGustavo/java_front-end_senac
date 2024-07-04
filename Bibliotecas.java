be17c1b8d64ecb9ea911e8ceb8879af1deb72b05
Gustavo Valença Marcilio <workgustavo10@gmail.com>
2024-07-04 16:29:19 -0300

    Add files via upload

---
+++ Bibliotecas.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Bibliotecas extends JPanel {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Color.WHITE);
+
+    }
+
+}
