a56833eb5dacc2d5246d3ecfe70e305fc21a32f9
Gustavo Valença Marcilio <workgustavo10@gmail.com>
2024-07-26 17:02:15 -0300

    Add files via upload

---
+++ ensaio-festa-junina.js
@@ -0,0 +1,69 @@
+/** 
+ * criar os objetos do ensaio
+*/
+
+/** 
+ * 
+ * Apolônio 3
+ * Gregório 1
+ * Rodrigo 2
+ * Natalia 7
+ * Aleciane 12
+ * Erik 1
+ * Rayssa 4
+ * 
+ * */
+
+class Pessoas {
+    constructor (musicalidade, corpo, idade, peso, cor, coordenacao_motora, altura, ritmo, nome, ano_nascimento, mes_nascimento, dia_nascimento, nome_pai, nome_mae, rg, cpf, endereco, numero_endereco, bairro, cidade, estado, cep, whatsapp, facebook, instagram, twiter_x, linkedin, email, tiktok, youtube, snapchat, orkut, tinder, uber, app_99, profissao, nacionalidade, estado_civil, possui_deficiencia, cnh, passaporte, religiao, partido_politico, idioma, figurino, ) {
+    }
+}
+
+class Musica {
+    constructor (ritmo, velocidade, compasso, artista, nome, ano, tempo, idioma, tipo, genero, autor, compositor, musica, letra, instrumentos, volume, vocalista, baterista, guitarrista, baixista, sonoplasta) {
+
+    }
+}
+
+class Ritmo {
+    constructor (movimento, tempo, velocidade, som, compasso, coreografia, nome, quantidade_de_pessoas, musica, bpm, sincronia, harmonia, vestimentas, genero_musical, maquiagens, equipamentos, local_de_ensaio, melodia, composicao, compositor, instrumentos) {
+        // Ação ...
+    }
+}
+
+class LocalDoEnsaio {
+    constructor (espaco, largura, altura, profundidade, energia, som, musica, microfone, palco, cadeira, capacidade_de_pessoas, equipamentos_de_luz, internet, cabo_de_energia, apresentacoes, stands, disponibilidade, seguranca, horario_de_funcionamento, acessibilidade, publico, artistas, janela, porta, camarim, comida, sala_de_descanso, escadas, ) {
+
+    }
+}
+
+class Banheiro {
+    constructor (genero, produtos, torneira, piz, vaso, piso, agua, energia, espelho, toalha_de_papel, suporte_de_sabonete, sabonete, suporte_de_bolsa, lixeira, descarga, mictorio, encanamento, odorizante, lampada, porta, interruptor, sensor_de_luz, janela, macaneta, batente) {
+
+    }
+}
+
+/**
+ *      this.musicalidade = musicalidade;
+        this.corpo = corpo;
+        this.idade = idade;
+        this.peso = peso;
+        this.cor = cor;
+        this.coordenacao_motora = coordenacao_motora;
+        this.altura = altura;
+        this.ritmo = ritmo;
+        this.nome = nome;
+        this.ano_nascimento = ano_nascimento;
+        this.mes_nascimento = mes_nascimento;
+        this.dia_nascimento = dia_nascimento;
+        this.nome_pai = nome_pai;
+        this.nome_mae = nome_mae;
+        this.rg = rg;
+        this.endereco = endereco;
+        this.numero_endereco = numero_endereco;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+
+ */
---
+++ ensaio-parana-junino.js
@@ -0,0 +1,72 @@
+/**
+ * Identificação de objetos pelos alunos do segundo ano do ensino médio Sesc/Senac 2024
+ * 
+ * Objetos:
+ * 
+ * Pessoa:
+ * 
+ * Parâmetros de uma Pessoa:
+ * 
+ * movimento, audicao, toque_fisico, cheiro, sabor, visao, idade, tamanho, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe;
+ * 
+ * Palco
+ * 
+ * Parâmetros do Palco:
+ * 
+ * tamanho, altura, largura, profundidade, espaco, vazio, piso, teto, cortina, iluminacao, som, energia, camarim, escada, projecao;
+ * 
+ * Musica
+ * 
+ * Parâmetros de Musica:
+ * 
+ * som, ritmo, sentimento, letra, batida, tempo, genero, melodia, harmonia, nota, timbre, instrumentos, vocal, acorde, baterista, guitarrista, pianista, banda, compasso;
+ * 
+ * Danca
+ * 
+ * Parâmetros de Danca:
+ * 
+ * ritmo, gingado, molejo, passos, coreografia, ensaio, tempo, estilo, dancarinos, energia;
+ * 
+ * Lugar
+ * 
+ * Parâmetros de Lugar:
+ * 
+ * espaco, aberto, fechado, vazio, cheio, clima, pessoas, luz, publico, privado;
+ * 
+ * Coreografia
+ * 
+ * Parâmetros de Coreografia:
+ * 
+ * passos, musica, figurino, ritmo, pessoas, sentimento, conexao, relacionamento;
+ * 
+ * Ritmo
+ * 
+ * Parâmetros de Ritmo:
+ * 
+ * tempo, musica, molejo, feeling, harmonia, batida, compasso;
+ * 
+ * Cadeira
+ * 
+ * Parâmetros de Cadeira:
+ * 
+ * assento, encosto, pernas, x, y, cor, pezinhos, material, estofado, almofada, tecido, design;
+ * 
+ * Luz
+ * 
+ * Parâmetros de Luz:
+ * 
+ * lampada, clima, temperatura, cor, intensidade, 
+ * 
+ * Janela
+ * Porta
+ * Caixa de som
+ * ArCondicionado
+ * Cortina
+ * Piso
+ * Teto
+ * Lampada
+ * Mesa
+ * Celular
+ * Corrimao
+ * 
+ */
